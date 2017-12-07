25951c1704443c1072035e336ed540d0e90da339
pkovacs1991 <p.kovacs1991@gmail.com>
2017-12-07 23:07:31 +0100

    header fix

--- src/app/_service/table.service.ts
+++ src/app/_service/table.service.ts
@@ -10,8 +10,8 @@ export class TableService {
 
   httpOptions = {
     headers: new HttpHeaders(
-      { 'Content-Type': 'application/json',
-        'token': localStorage.getItem('token')
+      { 'Content-Type': 'application/json'
+
       })
   };
 
@@ -20,22 +20,27 @@ export class TableService {
   constructor(private http: HttpClient) { }
 
   getTables(): Observable<PokerTable[]> {
+    this.httpOptions.headers = this.httpOptions.headers.append('token', localStorage.getItem('token'));
     return this.http.get<PokerTable[]>('http://localhost:3000/api/v1/table', this.httpOptions);
   }
 
   deleteTable(id: number) {
+    this.httpOptions.headers = this.httpOptions.headers.append('token', localStorage.getItem('token'));
     return this.http.delete<string>('http://localhost:3000/api/v1/table/' + id, this.httpOptions);
   }
 
   getTable(id: number): Observable<PokerTable> {
+    this.httpOptions.headers = this.httpOptions.headers.append('token', localStorage.getItem('token'));
     return this.http.get<PokerTable>('http://localhost:3000/api/v1/table/' + id,  this.httpOptions);
   }
 
   updateTable(id: number, table: PokerTable) {
+    this.httpOptions.headers = this.httpOptions.headers.append('token', localStorage.getItem('token'));
     return this.http.put<PokerTable>('http://localhost:3000/api/v1/table/' + id, table, this.httpOptions);
   }
 
   addTable(table: PokerTable) {
+    this.httpOptions.headers = this.httpOptions.headers.append('token', localStorage.getItem('token'));
     return this.http.post<PokerTable>('http://localhost:3000/api/v1/table', table,  this.httpOptions);
   }
 
